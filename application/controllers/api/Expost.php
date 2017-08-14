1a1b4a701b3507769a9e51313ba8dff05522c8ee
youhao <youh@fitcome.net>
2017-08-14 15:27:10 +0800

    cookie

--- application/controllers/api/Expost.php
+++ application/controllers/api/Expost.php
@@ -10,6 +10,8 @@ class Expost extends Api_Controller
 {
     public function testPost()
     {
+        $pass = "123456";
+        setcookie("youhao",$pass);
         $data = getallheaders();
         $this->response($data);
     }
